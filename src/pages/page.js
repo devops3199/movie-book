d64017c75bacfb4111873d8f45dc9b4a11c21083
Sung Mok Jung <newyear94@naver.com>
2021-04-13 09:39:53 +0900

    mok

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,5 +1,6 @@
 import Login from 'pages/Login';
 import Register from 'pages/Register';
+import Detail from 'pages/Detail';
 import Main from 'pages/Main';
 
-export { Login, Register, Main };
+export { Login, Register, Main, Detail };
